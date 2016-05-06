e2a9b806ec08f9ba4ca5aac9271f93c3635b928f
Ryan Southgate <ryansouthgate@live.com>
2016-05-06 11:35:40 +0100

    initial add

---
+++ WebJob1/AutofacActivator.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Microsoft.Azure.WebJobs.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJob1
+{
+    public class AutofacActivator : IJobActivator
+    {
+        private readonly IContainer _container;
+
+        public AutofacActivator(IContainer container)
+        {
+            _container = container;
+        }
+
+        public T CreateInstance<T>()
+        {
+            return _container.Resolve<T>();
+        }
+    }
+}
---
+++ WebJob1/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+
+namespace WebJob1
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+        }
+    }
+}
